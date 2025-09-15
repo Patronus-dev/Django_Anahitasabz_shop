585defaf2e56eb29168847a4ca9805ba8d7d0a07
Mehdi <mehdii.dev@gmail.com>
2025-09-15 23:33:43 +0330

    Similar Products Section added/Pagination created for products list

--- products/models.py
+++ products/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Keyword(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name=_("Keyword"))
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255, verbose_name=_("Product Name"))
     description = models.TextField(verbose_name=_("Description"), blank=True, null=True)
     price = models.PositiveIntegerField(default=0, verbose_name=_("Price"))
     active = models.BooleanField(default=True, verbose_name=_("Active"))
+    keywords = models.ManyToManyField(Keyword, blank=True, verbose_name=_("Keywords"))
 
     cover = models.ImageField(upload_to="products/", blank=True, null=True, verbose_name=_("Cover Image"))
     image1 = models.ImageField(upload_to="products/gallery/", blank=True, null=True, verbose_name=_("Image 1"))
@@ -22,3 +30,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_images(self):
+        images = []
+        if self.cover:
+            images.append(self.cover.url)
+        if self.image1:
+            images.append(self.image1.url)
+        if self.image2:
+            images.append(self.image2.url)
+        if self.image3:
+            images.append(self.image3.url)
+        return images
--- products/views.py
+++ products/views.py
@@ -6,8 +6,20 @@ class ProductListView(ListView):
     model = Product
     template_name = "products/product_list.html"
     context_object_name = "products"
-    queryset = Product.objects.filter(active=True).order_by("-datetime_created")
     paginate_by = 12
+
+    def get_queryset(self):
+        return Product.objects.filter(active=True).order_by('-datetime_created')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_obj = context['page_obj']
+
+        start = max(page_obj.number - 2, 1)
+        end = min(page_obj.number + 2, paginator.num_pages)
+        context['page_range_limited'] = range(start, end + 1)
+        return context
 
 
 class ProductDetailView(DetailView):
@@ -17,3 +29,20 @@ class ProductDetailView(DetailView):
 
     def get_queryset(self):
         return Product.objects.filter(active=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.object
+
+        # محصولات مشابه بر اساس کلیدواژه‌ها
+        similar_products = Product.objects.filter(
+            keywords__in=product.keywords.all()
+        ).exclude(pk=product.pk).distinct()[:4]
+
+        # اگر محصول مشابه وجود نداشت، آخرین محصولات فعال به غیر از محصول جاری
+        if not similar_products.exists():
+            similar_products = Product.objects.filter(active=True).exclude(pk=product.pk).order_by('-datetime_created')[
+                               :4]
+
+        context['similar_products'] = similar_products
+        return context
