3148790d61df20d13fbc5cd231c4b418669f0780
Mehdi <mehdii.dev@gmail.com>
2025-09-26 20:16:28 +0330

    For Contact us page: Site owner informations added

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import SiteContactInfo
 
-# Register your models here.
+
+@admin.register(SiteContactInfo)
+class SiteContactInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'instagram_id', 'whatsapp_number', 'telegram_id')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'phone_number')
+        }),
+        ('Social Media Links', {
+            'fields': ('instagram_id', 'whatsapp_number', 'telegram_id')
+        }),
+    )
+
+    # اگر میخوای فقط یک رکورد داشته باشه، میتونی اضافه کردن جدید رو محدود کنی
+    def has_add_permission(self, request):
+        return not SiteContactInfo.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class SiteContactInfo(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    phone_number = models.CharField(
+        max_length=10,
+        verbose_name=_("Phone Number"),
+        help_text=_("Enter your phone number")
+    )
+    instagram_id = models.CharField(verbose_name=_("Instagram Id"), blank=True, null=True)
+    whatsapp_number = models.IntegerField(verbose_name=_("WhatsApp Number"),
+                                          help_text=_("Enter your phone number without 0"), blank=True, null=True)
+    telegram_id = models.URLField(verbose_name=_("Telegram Id"), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Site Contact Info")
+        verbose_name_plural = _("Site Contact Info")
+
+    def __str__(self):
+        return self.name
